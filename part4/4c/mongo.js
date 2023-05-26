b60ae027952d0a5f7ea400c6944b9c570f4cb939
Salo M <69745872+salo18@users.noreply.github.com>
2023-05-26 18:50:36 +1200

    4c

---
+++ part4/4c/mongo.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const url =
+  `mongodb+srv://salocode14:${process.env.MONGO}@cluster0.zgskgi1.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'Mongoose makes things easy',
+  date: new Date(),
+  important: true,
+})
+
+/*
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
+*/
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
